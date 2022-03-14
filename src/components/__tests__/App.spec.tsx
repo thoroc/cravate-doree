452899ce03839dcbdf286ddf75f3d09c67a19e2e
Thomas Roche <thomas.roche@postcodelottery.co.uk>
2022-03-14 17:33:54 +0000

    type hinting on tests

--- src/components/__tests__/App.spec.tsx
+++ src/components/__tests__/App.spec.tsx
@@ -20,9 +20,9 @@ test('App Component can change toggle status with clicking toggle switch', async
 
   fireEvent.click(toggleButton)
   expect(container).toHaveTextContent(/the button is on/i)
-  expect(toggleButton).toSatisfy((button) => button.checked)
+  expect(toggleButton).toSatisfy((button: any) => button.checked)
 
   fireEvent.click(toggleButton)
   expect(container).toHaveTextContent(/the button is off/i)
-  expect(toggleButton).toSatisfy((button) => !button.checked)
+  expect(toggleButton).toSatisfy((button: any) => !button.checked)
 })
